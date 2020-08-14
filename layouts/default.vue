bbe4ba0011163607242d08c548e01fe5dff7caea
Hodosov <Hodosov.y@mail.ru>
2020-08-14 18:59:04 +0300

    add error page and scroll

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,7 +49,6 @@ export default {
           this.$router.push('/?message=leftChat')
           this.clearData()
       })
-     
     }
   }
 }
---
+++ layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+<v-app app>
+    <v-main>
+    <v-container fluid>
+      <h1>Страница не найдена</h1>
+    </v-container>
+  </v-main>
+</v-app>
+</template>
